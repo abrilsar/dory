74f799213e7a0daac24506aa26ad70bc13289246
abrilsar <a.sar@correo.unimet.edu.ve>
2024-06-14 19:03:36 -0400

    fix

--- apps/backoffice/src/components/example-connect-backend/ReactQueryClientExample.tsx
+++ apps/backoffice/src/components/example-connect-backend/ReactQueryClientExample.tsx
@@ -1,17 +1,17 @@
-"use client";
+// "use client";
 
-import React from "react";
-import { useUsers } from "@/services/user/hooks";
+// import React from "react";
+// import { useUsers } from "@/services/user/hooks";
 
-interface ReactQueryClientProps {
-  users: any; // FIXME: Replace with the correct type
-}
+// interface ReactQueryClientProps {
+//   users: any; // FIXME: Replace with the correct type
+// }
 
-function ReactQueryClientExample({ users }: ReactQueryClientProps) {
-  // if you need the same query in client and server, you can use initialData
-  const { data, isLoading } = useUsers({});
+// function ReactQueryClientExample({ users }: ReactQueryClientProps) {
+//   // if you need the same query in client and server, you can use initialData
+//   const { data, isLoading } = useUsers({});
 
-  return <div>Data in Client: {JSON.stringify(data)}</div>;
-}
+//   return <div>Data in Client: {JSON.stringify(data)}</div>;
+// }
 
-export default ReactQueryClientExample;
+// export default ReactQueryClientExample;
