a3d28906ae068e7d62859bfbfd56e4a2b7c60a75
PaolaMarquez <paola.marquez@correo.unimet.edu.ve>
2024-06-15 10:57:09 -0400

    install googleapis

--- apps/api/src/components/drive-service/drive.service.ts
+++ apps/api/src/components/drive-service/drive.service.ts
@@ -1,75 +1,75 @@
-// const { google } = require('googleapis');
+const { google } = require('googleapis');
 
-// const credentials = require('./credentials.json');
+const credentials = require('./credentials.json');
 
-// const auth = new google.auth.GoogleAuth({
-//     credentials: credentials,
-//     scopes: ['https://www.googleapis.com/auth/drive.file']
-// });
+const auth = new google.auth.GoogleAuth({
+    credentials: credentials,
+    scopes: ['https://www.googleapis.com/auth/drive.file']
+});
 
-// const drive = google.drive({ version: 'v3', auth });
+const drive = google.drive({ version: 'v3', auth });
 
 async function uploadFile(args: any) {
     try {
-        // const fileMetaData = {
-        //     name: `${args.name}.txt`,
-        //     parents: ['1D29xwOirXzBMOR7yRqmVYzvNzcsf5pYQ']
-        // }
+        const fileMetaData = {
+            name: `${args.name}.txt`,
+            parents: ['1D29xwOirXzBMOR7yRqmVYzvNzcsf5pYQ']
+        }
 
-        // const mediaFile = {
-        //     body: args.info,
-        //     mimeType: 'text/plain'
-        // }
-        // const file = await drive.files.create({
-        //     resource: fileMetaData,
-        //     media: mediaFile,
-        //     fields: 'id'
-        // })
-        // const fileId = file.data.id;
-        // return fileId
-        return '1622YB5zAwBaUX1ILJVgrduTSnUic8tI6'
+        const mediaFile = {
+            body: args.info,
+            mimeType: 'text/plain'
+        }
+        const file = await drive.files.create({
+            resource: fileMetaData,
+            media: mediaFile,
+            fields: 'id'
+        })
+        const fileId = file.data.id;
+        return fileId
+        // return '1622YB5zAwBaUX1ILJVgrduTSnUic8tI6'
     } catch (error) {
     }
 }
 async function readFile(args: any) {
-    // const { data } = await drive.files.get({
-    //     fileId: args.fileId,
-    //     alt: 'media'
-    // });
-    // return data
-    return 'Hola como estas'
+    const { data } = await drive.files.get({
+        fileId: args.fileId,
+        alt: 'media'
+    });
+    return data
+    // return 'Hola como estas'
 }
 
 async function updateFileContent(args: any) {
-    // const fileId = args.fileId as string
-    // const newContent = args.newContent as string
-    // console.log("Nuevo contenido: ", newContent)
+    const fileId = args.fileId as string
+    const newContent = args.newContent as string
+    console.log("Nuevo contenido: ", newContent)
     try {
-        // const { data: existingFile } = await drive.files.get({
-        //     fileId,
-        //     fields: 'name, mimeType, size'
-        // });
+        const { data: existingFile } = await drive.files.get({
+            fileId,
+            fields: 'name, mimeType, size'
+        });
 
-        // const fileMetadata = {
-        //     name: existingFile.name,
-        //     mimeType: existingFile.mimeType
-        // };
+        const fileMetadata = {
+            name: existingFile.name,
+            mimeType: existingFile.mimeType
+        };
 
-        // const media = {
-        //     mimeType: 'text/plain',
-        //     body: newContent
-        // };
+        const media = {
+            mimeType: 'text/plain',
+            body: newContent
+        };
 
-        // const { data: updatedFile } = await drive.files.update({
-        //     fileId,
-        //     resource: fileMetadata,
-        //     media: media,
-        //     fields: 'id, name, mimeType, size'
-        // });
+        const { data: updatedFile } = await drive.files.update({
+            fileId,
+            resource: fileMetadata,
+            media: media,
+            fields: 'id, name, mimeType, size'
+        });
 
-        // console.log(`Archivo actualizado: ${updatedFile.name} (${updatedFile.size} bytes)`);
-        // return updatedFile;
-        return 'updatedfile'
+        console.log(`Archivo actualizado: ${updatedFile.name} (${updatedFile.size} bytes)`);
+        return updatedFile;
+        // return 'updatedfile'
     } catch (error) {
         console.error('Error al actualizar el archivo:', error);
         throw error;
@@ -79,11 +79,11 @@ async function updateFileContent(args: any) {
 
 async function deleteFile(args: any) {
     try {
-        // await drive.files.delete({
-        //     fileId: args.fileId
-        // });
-        // console.log(`Archivo borrado: ${args.fileId}`);
-        return 'Se elimino'
+        await drive.files.delete({
+            fileId: args.fileId
+        });
+        console.log(`Archivo borrado: ${args.fileId}`);
+        // return 'Se elimino'
     } catch (error) {
         console.error('Error al borrar el archivo:', error);
         throw error;
